e00b26cb53173b68e6fd7e3b6ff30cb61139c40b
Adèle PLUQUET <apluquet@ircam.fr>
2022-01-28 11:02:02 +0100

    callbacks

--- rotaryEncoder.cpp
+++ rotaryEncoder.cpp
@@ -12,88 +12,26 @@ static int _t2 = 0;
 static int _turning2 = 0;
 static unsigned long _debounceDelay_ = 50;
 
-static void data1_callback()
+int i = 0;
+int j = 0;
+
+static void callback1()
 {
-    cout << "coucou0" << endl;
-	if (!_turning1)
-	{	
-		_turning1 = -1;
-		_t1 = millis();
-		return;
-	}
-	//clock before!
-	if ( millis() - _t1 > _debounceDelay_ )
-	{
-		//GlView::getInstance()->update("r1", -1);
-        cout << "turning 1 left" << endl;
-		_turning1 = 0;
-		_t1 = 0;
-		
-	}
+    cout << "callback 1 -> " << i << endl;
+    i++;
 }
 
-static void clock1_callback()
+static void callback2()
 {
-    cout << "coucou1" << endl;
-	if (!_turning1)
-	{	
-		_turning1 = 1;
-		_t1 = millis();
-		return;
-	}
-	//data before!
-	if ( millis() - _t1 > _debounceDelay_ )
-	{
-		//GlView::getInstance()->update("r1", 1);
-        cout << "turning 1 right" << endl;
-		_turning1 = 0;
-		_t1 = 0;
-	}
-}
-
-static void data2_callback()
-{
-    cout << "coucou2" << endl;
-	if (!_turning2)
-	{	
-		_turning2 = -1;
-		_t2 = millis();
-		return;
-	}
-	//clock before!
-	if ( millis() - _t2 > _debounceDelay_ )
-	{
-        //GlView::getInstance()->update("r2", -1);
-        cout << "turning 2 left" << endl;
-		_turning2 = 0;
-		_t2 = 0;
-	}
-}
-
-static void clock2_callback()
-{
-    cout << "coucou3" << endl;
-	if (!_turning2)
-	{	
-		_turning2 = 1;
-		_t2 = millis();
-		return;
-	}
-	//data before!
-	if ( millis() - _t2 > _debounceDelay_ )
-	{
-		//GlView::getInstance()->update("r2", 1);
-        cout << "turning 2 right" << endl;
-		_turning2 = 0;
-		_t2 = 0;
-	}
+    cout << "callback 2 -> " << j << endl;
+    j++;
 }
 
 RotaryEncoder::RotaryEncoder(int dataPin, int clkPin, int pushPin, std::string name)
 {
-	static bool once = false;
-	if (!once)
-    { 
+    static bool once = false;
+    if (!once)
+    {
         wiringPiSetupGpio();
         once = true;
     }
@@ -110,26 +48,20 @@ void RotaryEncoder::setup()
     pinMode(dataPin_, INPUT);
     pinMode(clkPin_, INPUT);
     pinMode(pushPin_, INPUT);
-    if ( name_ == "1" )
+    if (name_ == "1")
     {
-    	int err = 0;
-    	err = wiringPiISR(dataPin_,INT_EDGE_RISING,data1_callback); 
-    	cout << err << endl;
-    	err = wiringPiISR(clkPin_,INT_EDGE_RISING,clock1_callback);
-    	cout << err << endl; 
+        int err = 0;
+        err = wiringPiISR(dataPin_, INT_EDGE_RISING, callback1);
+        cout << "err CB1 = " << err << endl;
     }
-    else if ( name_ == "2" )
+    else if (name_ == "2")
     {
-    	int err = 0;
-    	err = wiringPiISR(dataPin_,INT_EDGE_RISING,data2_callback); 
-    	cout << err << endl;
-    	err = wiringPiISR(clkPin_,INT_EDGE_RISING,clock2_callback); 
-    	cout << err << endl;
+        int err = 0;
+        err = wiringPiISR(dataPin_, INT_EDGE_RISING, callback2);
+        cout << "err CB2 = " << err << endl;
     }
-
-    cout << "hello" << endl;
 }
-
+/*
 int RotaryEncoder::debounce()
 {
     int ret = 0;
@@ -158,35 +90,23 @@ int RotaryEncoder::debounce()
     lastPushState_ = reading;
     return ret;
 }
-/*
-void RotaryEncoder::rotate()
-{
-    if (lastDataState_ == 0 && currentDataState_ == 1)
-    {
-        counter_--;
-        cout << "counter " << name_ << " = " << counter_ << endl;
-        direction_ = -1;
-    }
-
-    if (lastDataState_ == 1 && currentDataState_ == 0)
-    {
-        counter_++;
-        cout << "counter " << name_ << " = " << counter_ << endl;
-        direction_ = 1;
-    }
-}*/
-
+*/
 int main()
 {
     wiringPiSetupGpio();
-    RotaryEncoder A = RotaryEncoder(13, 11, 15, "A");
-    RotaryEncoder B = RotaryEncoder(22, 31, 7, "B");
+    RotaryEncoder A = RotaryEncoder(13, 11, 15, "1");
+    RotaryEncoder B = RotaryEncoder(22, 31, 7, "2");
 
     cout << "You can start" << endl;
-
+    int k = 0;
     while (1)
     {
-	    delay(50);
+        k++;
+        delay(50);
+        if (k > 1000)
+        {
+            j = 0;
+            i = 0;
+        }
     }
 }
-
